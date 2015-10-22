e054cf5270e18c91f6bfec74e51cf08fd9efa8b0
dlevasseur <dominic.levasseur@wazeedigital.com>
2015-10-22 12:36:32 -0600

    Assignment 2 source file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions create an interface to a "matrix" object that caches
+## its own inverse. After creating or re-setting the value of the matrix
+## object, the first call to retrieve the inverse will calculate
+## the inverse of the matrix. Subsequent calls with retrieve the cached
+## value.
 
-## Write a short comment describing this function
+## The makeCacheMatrix function will create the matrix object interface.
+## The function argument x should be an invertible matrix.
+## The caller should use the set function to change the underlying matrix
+## object to a different invertible matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function calculates, if necessary, and returns the 
+## inverse of the underlying matrix object stored in the makeCacheMatrix
+## matrix interface.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
